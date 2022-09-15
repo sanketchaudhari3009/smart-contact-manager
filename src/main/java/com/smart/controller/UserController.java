e5030f6b3a8b8d6c1d6fb4416180cac05be6ffe7
Sanket Chaudhari <78204805+sanketchaudhari3009@users.noreply.github.com>
2022-09-15 21:23:00 +0530

    user dashboard modifications

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,15 +1,35 @@
 package com.smart.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	@RequestMapping("/index")
-	public String dashboard()
+	public String dashboard(Model model, Principal principal)
 	{
+		String userName = principal.getName();
+		System.out.println("username"+userName);
+		
+		User user = userRepository.getUserByUserName(userName);
+		
+		System.out.println("user "+user);
+		
+		model.addAttribute("user", user);
+		
 		return "normal/user_dashboard";
 	}
 }
