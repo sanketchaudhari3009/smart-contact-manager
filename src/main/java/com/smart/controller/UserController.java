7938e186a5ebc59e7ea6abc14e901ebbcef87125
Sanket Chaudhari <78204805+sanketchaudhari3009@users.noreply.github.com>
2022-09-16 17:24:31 +0530

    view contact

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.smart.dao.ContactRepository;
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
@@ -32,6 +34,9 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private ContactRepository contactRepository;
 	
 	@ModelAttribute
 	public void addCommonData(Model model, Principal principal)
@@ -111,6 +116,24 @@ public class UserController {
 			session.setAttribute("message", new Message("Something went wrong!! Try again..", "danger"));
 		}
 		return "normal/add_contact_form";
+	}
+	
+	
+	//show contact handler
+	@GetMapping("/show-contacts")
+	public String showContacts(Model m, Principal principal)
+	{
+		m.addAttribute("title", "Show user contacts");
+		
+		//send contact list
+		String userName = principal.getName();
+		User user = this.userRepository.getUserByUserName(userName);
+		List<Contact> contacts = this.contactRepository.findContactsByUser(user.getId());
+		
+		m.addAttribute("contacts", contacts);
+		
+		
+		return "normal/show_contacts";
 	}
 	
 }
