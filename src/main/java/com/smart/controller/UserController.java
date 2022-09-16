aca581700c8db4bbaa361c79eb555206578f81e2
Sanket Chaudhari <78204805+sanketchaudhari3009@users.noreply.github.com>
2022-09-16 12:23:40 +0530

    image contact issue resolved

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,14 +1,22 @@
 package com.smart.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
@@ -53,16 +61,50 @@ public class UserController {
 	}
 	
 	@PostMapping("/process-contact")
-	public String processContact(@ModelAttribute Contact contact, Principal principal )
+	public String processContact(@ModelAttribute Contact contact, @RequestParam("profileImage") MultipartFile file, Principal principal )
 	{
+		
+		try
+		{
 		String name = principal.getName();
 		User user = this.userRepository.getUserByUserName(name);
-		contact.setUser(user);
+		
+		//processing uploading file
+		if(file.isEmpty())
+		{
+			System.out.println("File is empty");
+		}
+		else
+		{
+			contact.setImage(file.getOriginalFilename());
+			
+			File saveFile = new ClassPathResource("static/img").getFile();
+			
+			Path path = Paths.get(saveFile.getAbsolutePath()+File.separator+file.getOriginalFilename());
+			
+			Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+			
+			System.out.println("Image is uploaded");
+		}
+		
+		
 		user.getContacts().add(contact);
+		contact.setUser(user);
+		
+		
+		
+		
+		
 		this.userRepository.save(user);
 		
 		System.out.println("DATA"+contact);
 		System.out.println("added to DB");
+		}
+		catch(Exception e)
+		{
+			System.out.println("ERROR " +e.getMessage());
+			e.printStackTrace();
+		}
 		return "normal/add_contact_form";
 	}
 	
