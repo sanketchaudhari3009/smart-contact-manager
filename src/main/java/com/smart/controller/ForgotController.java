2ed08beff19dbd272b8fa5bdfbbc21d4a52cc98c
Sanket Chaudhari <78204805+sanketchaudhari3009@users.noreply.github.com>
2022-09-18 09:14:28 +0530

    email sending issue resolved

--- src/main/java/com/smart/controller/ForgotController.java
+++ src/main/java/com/smart/controller/ForgotController.java
@@ -2,15 +2,23 @@ package com.smart.controller;
 
 import java.util.Random;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.smart.service.EmailService;
+
 @Controller
 public class ForgotController {
 	
 	Random random = new Random(1000);
+	
+	@Autowired
+	private EmailService emailService;
 
 	//email-id form open handler
 	@RequestMapping("/forgot")
@@ -21,7 +29,7 @@ public class ForgotController {
 	
 	
 	@PostMapping("/send-otp")
-	public String sendOTP(@RequestParam("email") String email)
+	public String sendOTP(@RequestParam("email") String email, HttpSession session)
 	{
 		System.out.println("email: "+email);
 		
@@ -35,7 +43,24 @@ public class ForgotController {
 		
 		//send otp functionality
 		
-		return "verify_otp";
+		String subject = "OTP from SCM";
+		String message = "OTP = "+otp;
+		String to = email;
+		
+		boolean flag = this.emailService.sendEmail(subject, message, to);
+		
+		if(flag)
+		{
+			session.setAttribute("otp", otp);
+			return "verify_otp";
+		}
+		else
+		{
+			session.setAttribute("message", "Check your email id !!");
+			return "forgot_email_form";
+		}
+		
+		
 	}
 	
 }
